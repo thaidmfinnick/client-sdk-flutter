91f419cf613cca8824827eb4908b5db2373a2357
finnick <thai.dm279@gmail.com>
2024-01-16 10:42:01 +0700

    improve: update package

--- lib/src/support/app_state.dart
+++ lib/src/support/app_state.dart
@@ -14,14 +14,14 @@
 
 import 'dart:async';
 
-import 'package:flutter_window_close/flutter_window_close.dart';
+// import 'package:flutter_window_close/flutter_window_close.dart';
 
 class AppStateListener {
   AppStateListener._internal() {
-    FlutterWindowClose.setWindowShouldCloseHandler(() async {
-      onWindowShouldClose.add('close');
-      return true;
-    });
+    // FlutterWindowClose.setWindowShouldCloseHandler(() async {
+    //   onWindowShouldClose.add('close');
+    //   return true;
+    // });
   }
   static final AppStateListener instance = AppStateListener._internal();
   final StreamController<String> onWindowShouldClose =
